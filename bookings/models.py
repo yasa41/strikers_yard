9ecdaa7093e31f307d9d07d8fe489655c26fe4b2
Sheikh Umaid <sheikhumaid@pm.me>
2025-11-25 15:38:17 +0530

    added email to db schema

--- bookings/models.py
+++ bookings/models.py
@@ -35,6 +35,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     phone_number = models.CharField(max_length=15, unique=True)
     name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
